c00a3aa8236da7ed2e854f290a5e7210aceed78f
matus <matusmartiska24@gmail.com>
2024-12-29 21:23:42 +0100

    prihlasovanie do polovice spravene

--- assets/js/rozhodca/prihlasenie.js
+++ assets/js/rozhodca/prihlasenie.js
@@ -10,18 +10,34 @@ const getReferee = async () => {
     return data;
 };
 
+
 document.querySelector('form').addEventListener('submit',event =>{
     //aby sa stranka nerefreshovala po odoslani
     event.preventDefault();
 
+    const message = document.getElementById('message');
+
     getReferee()
-        .then(data =>{
-            /*tu zavolam funkciu na spracovanie vstupu, ktora
-            redirectne na dalsiu stranku alebo ukaze error message
-            */ 
+        .then(data => {
+            const input = document.getElementById('code').value;
+
+            //funkcionalita na skontrolovanie kodu s json suborom
+            const found = data.find(element => element.code === input);
+            /*
+              pôjde asi do fukcie, ktorá vráti true a zmení údaj rozhodcu .taken na true
+              a uloží id rozhodcu do localstorage
+              const found = findreferee();
+            */
+
+            if (found) {
+                window.location.href = 'dashboard.html';
+            } else {
+                message.innerText = 'Zadaný kód je neplatný!';
+                message.classList.replace('message-hidden', 'message');
+            }
         })
         .catch(err =>{
-            const message = document.getElementById('message');
+            //ak je chyba v pripojeni na server
             console.log(err);
             message.innerText = 'Chyba v požiadavke';
             message.classList.replace('message-hidden','message');
