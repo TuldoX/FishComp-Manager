e7d45e2b4193ba25beadd0a9402c16aa9f567570
matus <matusmartiska24@gmail.com>
2024-12-30 15:27:57 +0100

    prihlasovanie FE logic done

--- assets/js/rozhodca/prihlasenie.js
+++ assets/js/rozhodca/prihlasenie.js
@@ -1,37 +1,52 @@
 //funkcia na ziskanie udajov o rozhodcoch
 const getReferee = async () => {
     const response = await fetch('../../data/rozhodca-codes.json');
-
     if(response.status !== 200){
         throw new Error(`Response status: ${response.status}`);
     }
-
     const data = await response.json();
     return data;
 };
 
-
 document.querySelector('form').addEventListener('submit',event =>{
     //aby sa stranka nerefreshovala po odoslani
     event.preventDefault();
-
+    let userid = null;
     const message = document.getElementById('message');
 
     getReferee()
         .then(data => {
             const input = document.getElementById('code').value;
+            let found = false;
+            let sprava = null;
 
-            //funkcionalita na skontrolovanie kodu s json suborom
-            const found = data.find(element => element.code === input);
-            /*
-              pôjde asi do fukcie, ktorá vráti true a zmení údaj rozhodcu .taken na true
-              a uloží id rozhodcu do localstorage
-              const found = findreferee();
-            */
+            //logika na prihlasenie rozhodcu
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].code === input) {
+                    if (data[i].taken) {
+                        sprava = 'Zadaný rozhodca už je prihlásený!';
+                        found = true;
+                    } else {
+                        userid = data[i].id;
+                        found = true;
+                    }
+                    break;
+                }
+            }
 
-            if (found) {
+            if(found && sprava === null){
+                //tu by mala byt funkcia na update json suboru ale na to potrebujem backend - zmena .taken na true
+                //ulozenie id do sessionstorage aby som mohol retrievnut data na dashboard
+                sessionStorage.setItem('userId',userid);
                 window.location.href = 'dashboard.html';
-            } else {
+            }
+            else if(found && sprava !== null){
+                message.innerText = sprava;
+                message.classList.replace('message-hidden','message');
+                sprava = null;
+                found = false;
+            }
+            else{
                 message.innerText = 'Zadaný kód je neplatný!';
                 message.classList.replace('message-hidden', 'message');
             }
